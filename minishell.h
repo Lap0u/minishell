cb6334b2a8be3249b46e080a9adff4b163522b10
cbeaurai <cbeaurai@student.42.fr>
2021-10-21 22:16:41 +0200

    try read

--- minishell.h
+++ minishell.h
@@ -2,5 +2,15 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <stdlib.h>
+#include <unistd.h>
 
+int	ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
 
--- readline.c
+++ readline.c
@@ -1,10 +1,19 @@
 #include "minishell.h"
 //comp avec -lreadline
+#define B_SIZE 10
+#define PROMPT ">minishell "
 int	main()
 {
+	static char buffer[B_SIZE + 1];
+	write(1, PROMPT, ft_strlen(PROMPT));
+	read(0,  buffer, B_SIZE);
+	buffer[B_SIZE] = 0;
+	printf("%s\n", buffer);
+	/*
 	char *cmd;
 	cmd = readline(">minishell ");
 	printf("%s\n", cmd);
+	add_history(cmd);
 	free(cmd);///res de readline a free
-	return (0);
+	*/return (0);
 }
