4ddd7aad6b6f32e0947671cc3da930a5e54d5bd1
Clement BEAURAIN <cbeaurai@e1r1p14.42.fr>
2021-11-13 10:37:23 -0800

    ajout cd sans arg

--- builtin/cd.c
+++ builtin/cd.c
@@ -12,9 +12,37 @@
 
 #include "../minishell.h"
 
-void    ft_bi_cd(t_simple_command *c_table) //pb avec env absolu et sans arg //update des vars PWD OLD PWD etc voir man
+void    cd_noarg(t_simple_command *c_table)
+{
+    int i;
+
+    i = 0;
+    while (c_table->env[i])
+    {
+        if (ft_strncmp("HOME=", c_table->env[i], 5) == 0)
+        {
+            if (chdir(&c_table->env[i][5]) < 0)
+            {
+                c_table->last_ret = 1;
+                perror("cd");
+            }
+            else
+                c_table->last_ret = 0;
+            return ;
+        }
+        i++;
+    }
+    fprintf(stdout, "cd: HOME not set\n");
+    c_table->last_ret = 1;
+}
+void    ft_bi_cd(t_simple_command *c_table) //pb avec env absolu update des vars PWD OLD PWD etc voir man
 {
     int ret;
+    if (c_table->args_num < 1)
+    {
+        cd_noarg(c_table);
+        return ;
+    }
     ret = chdir(c_table->args[1]);
     if (ret == -1)
     {
