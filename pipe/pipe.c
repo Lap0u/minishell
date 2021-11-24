0256d220d30dadb73f3fd87a79965d3b9855bdcb
Clement BEAURAIN <cbeaurai@e2r6p12.clusters.42paris.fr>
2021-11-24 15:27:45 +0100

    compil

--- pipe/pipe.c
+++ pipe/pipe.c
@@ -112,7 +112,7 @@ int		ft_pipe(t_simple_command *c_table)
 			if (ft_isbuiltin(c_table->cmd))
 				ft_split_builtin(&c_table);
 			else	//les builtins et exec doivent renvoyer une valeur de retour pour $?
-				ft_exec_bin(c_table, c_table->env, pipefd, i);
+				ft_exec_bin(c_table, c_table->env);
 		}
 		i++;
 		c_table = c_table->next;
