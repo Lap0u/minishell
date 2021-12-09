64fd38a630bac5ac23e889e0ff8ee4e16762a344
Clement BEAURAIN <cbeaurai@e2r7p20.clusters.42paris.fr>
2021-12-09 17:32:48 +0100

    norme ok

--- parsing/parser/fill_redir_parser.c
+++ parsing/parser/fill_redir_parser.c
@@ -6,7 +6,7 @@
 /*   By: cbeaurai <cbeaurai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 12:06:57 by cbeaurai          #+#    #+#             */
-/*   Updated: 2021/12/09 14:46:37 by cbeaurai         ###   ########.fr       */
+/*   Updated: 2021/12/09 16:22:26 by cbeaurai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,25 +34,6 @@ int	is_expandable(char *str, int red, int quote, char **env)
 		i++;
 	}
 	return (0);
-}
-
-int	is_there_red(t_token *tok, int ind, int len, char **env)
-{
-	while (ind < len && tok[ind].type != PIPE)
-	{
-		if (tok[ind].type == RED_HERE_DOC)
-			return (0);
-		else if (tok[ind].type >= RED_OUT
-			&& tok[ind].type <= RED_HERE_DOC
-			&& tok[ind + 1].fl_quotes == 1)
-			return (0);
-		else if (tok[ind].type >= RED_OUT
-			&& tok[ind].type <= RED_HERE_DOC
-			&& is_expandable(tok[ind + 1].value, 0, 0, env))
-			return (0);
-		ind++;
-	}
-	return (1);
 }
 
 void	new_redir(t_token *tok, t_redir **start)
@@ -84,32 +65,18 @@ int	first_redir(t_redir **start, t_token *tok, int ind)
 	return (-1);
 }
 
-int	is_good_redir(t_token *tok, int ind, char **env)
-{
-	if (tok[ind].type >= RED_OUT && tok[ind].type <= RED_HERE_DOC
-		&& is_expandable(tok[ind + 1].value, tok[ind].type,
-			tok[ind + 1].fl_quotes, env))
-		return (1);
-	return (0);
-}
-
-char	*call_expand(t_token *tok, int ind, char **env, int ret)
-{
-	return (ft_expand_dollar(tok[ind + 1].value, tok[ind].type, env, ret));
-}
-
-t_redir	*ft_fill_redir(t_token *tok, int ind, int len, char **env, int ret)
+t_redir	*ft_fill_redir(t_token *tok, int ind, char **env, int ret)
 {
 	t_redir	*start;
 	int		bool_start;
 
 	bool_start = -1;
-	if (is_there_red(tok, ind, len, env) == 1)
+	if (is_there_red(tok, ind, tok[0].size, env) == 1)
 		return (NULL);
 	start = malloc(sizeof(t_redir));
 	if (start == NULL)
 		return (NULL);
-	while (ind < len && tok[ind].type != PIPE)
+	while (ind < tok[0].size && tok[ind].type != PIPE)
 	{
 		if (is_good_redir(tok, ind++, env))
 		{
