399569084fea4b6d62e2bb7e9ddc8c3d6667905d
Clement BEAURAIN <cbeaurai@e2r5p10.clusters.42paris.fr>
2021-12-10 11:10:20 +0100

    remove des printfs

--- builtin/export_utils.c
+++ builtin/export_utils.c
@@ -6,7 +6,7 @@
 /*   By: cbeaurai <cbeaurai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 13:53:04 by cbeaurai          #+#    #+#             */
-/*   Updated: 2021/12/03 16:42:00 by cbeaurai         ###   ########.fr       */
+/*   Updated: 2021/12/10 11:09:14 by cbeaurai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -106,7 +106,6 @@ void	ft_export_add(char *toadd, t_simple_command **c_table)
 	{
 		if (ft_strccmp(toadd, (*c_table)->env[i], '=') == 0)
 		{
-			printf("passe");
 			ft_export_replace(&toadd[len], toadd, c_table, i);
 			return ;
 		}
--- libft/error.c
+++ libft/error.c
@@ -6,7 +6,7 @@
 /*   By: cbeaurai <cbeaurai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 00:51:59 by cbeaurai          #+#    #+#             */
-/*   Updated: 2021/09/26 00:56:25 by cbeaurai         ###   ########.fr       */
+/*   Updated: 2021/12/10 11:09:59 by cbeaurai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,24 @@
 
 void	ft_wrong_file(void)
 {
-	printf("Error while opening file, exiting...\n");
+	write(2, "Error while opening file, exiting...\n", 38);
 	exit(EXIT_SUCCESS);
 }
 
 void	ft_wrong_arg(void)
 {
-	printf("Error, wrong argument, exiting...\n");
+	write(2, "Error, wrong argument, exiting...\n", 35);
 	exit(EXIT_SUCCESS);
 }
 
 void	ft_wrong_env(void)
 {
-	printf("Error, PATH variable couldn't be found, exiting...\n");
+	write(2, "Error, PATH variable couldn't be found, exiting...\n", 52);
 	exit(EXIT_SUCCESS);
 }
 
 void	ft_wrong_malloc(void)
 {
-	printf("Error, malloc couldn't allocate memory accordingly, exiting...\n");
+	write(2, "Error, malloc couldn't allocate memory accordingly, exiting...\n", 64);
 	exit(EXIT_SUCCESS);
 }
