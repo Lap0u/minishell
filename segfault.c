cadee32ef4f584f9d1c9f4dba0ba67bee641eb1c
Clement BEAURAIN <cbeaurai@e2r4p5.clusters.42paris.fr>
2021-12-20 10:47:06 +0100

    modif main segfault.c

--- segfault.c
+++ segfault.c
@@ -1,8 +1,19 @@
 #include <sys/types.h>
 #include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int	main()
 {
 	kill(0, SIGSEGV);
 	return (0);
 }
+
+// int main ()
+// {
+// 	char *str;
+// 	str = malloc(sizeof(char) * 2);
+// 	str[5] = 'r';
+// 	printf("%s", str);
+// 	return (0);
+// }
